e684d8047d9be75365da1974b51f9e346485367e
Casper Beyer <caspervonb@pm.me>
2021-05-11 02:00:12 +0000

    fix(test): allow doc tests to be checked when there are no test modules

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -19,6 +19,19 @@ use test_util as util;
 use tokio::task::LocalSet;
 
 #[test]
+fn dts_documentation_tests() {
+  let status = util::deno_cmd()
+    .arg("test")
+    .arg("--doc")
+    .arg(util::root_path().join("cli/dts/"))
+    .spawn()
+    .unwrap()
+    .wait()
+    .unwrap();
+  assert!(status.success());
+}
+
+#[test]
 fn js_unit_tests_lint() {
   let status = util::deno_cmd()
     .arg("lint")
